0c3b99d3d1636cd2f72c7a2308e9e89192208335
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-05 00:59:19 +0100

    Generate json with the information about the conversion and the original url

--- simplevideodownload.py
+++ simplevideodownload.py
@@ -4,36 +4,37 @@ import os
 import sys
 import shutil
 import subprocess
+import datetime
+import json
 
 
 
 for url in sys.argv[1:]:
-	print url
 	tempdir = tempfile.mkdtemp(dir=os.getcwd())
-	print tempdir
 	os.chdir(tempdir)
 	subprocess.call(["youtube-dl", url])
 
 	initial_path = os.listdir('.')[0]
 	subprocess.call(["rename", "s/ /_/g", initial_path])
 	final_path = initial_path.replace(' ', '_')
-	mp3_path = final_path + '.mp3'
+	base_path = final_path[:final_path.rfind('.')]
+	mp3_path = base_path + '.mp3'
 	
 	subprocess.call(["ffmpeg", "-i", final_path, mp3_path])
-	print initial_path
-	print final_path
-	print mp3_path
-	
+
+	data = {
+		'date':datetime.datetime.now(),
+		'url': url,
+		'final_path': final_path,
+		'mp3_path': mp3_path,
+	}
+
 	shutil.move(final_path, "../")
 	shutil.move(mp3_path, "../")
 
 	os.chdir('../')
-
-	print "EFA1", os.listdir('.')
-	print "EFA2", os.getcwd()
+	
+	with open(base_path + '.json', 'w') as f:
+		f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+	
 	shutil.rmtree(tempdir, ignore_errors=True)
-
-    
-
-    
-
