437b992a33b73d0b89e4e4d3882f1536fb3ecf85
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-04 13:58:21 +0100

    mp3 conversion

--- simplevideodownload.py
+++ simplevideodownload.py
@@ -5,17 +5,32 @@ import sys
 import shutil
 import subprocess
 
+
+
 for url in sys.argv[1:]:
 	print url
-	tempdir = tempfile.mkdtemp()
+	tempdir = tempfile.mkdtemp(dir=os.getcwd())
 	print tempdir
 	os.chdir(tempdir)
 	subprocess.call(["youtube-dl", url])
 
-	for root, dirs, files in os.walk('./'):
-		print root, dirs, files
+	initial_path = os.listdir('.')[0]
+	subprocess.call(["rename", "s/ /_/g", initial_path])
+	final_path = initial_path.replace(' ', '_')
+	mp3_path = final_path + '.mp3'
+	
+	subprocess.call(["ffmpeg", "-i", final_path, mp3_path])
+	print initial_path
+	print final_path
+	print mp3_path
+	
+	shutil.move(final_path, "../")
+	shutil.move(mp3_path, "../")
 
 	os.chdir('../')
+
+	print "EFA1", os.listdir('.')
+	print "EFA2", os.getcwd()
 	shutil.rmtree(tempdir, ignore_errors=True)
 
     
